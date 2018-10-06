e75302f75dd9e19c1fbbcc671f3057ab185b0eb2
Zhuowan Li <zhuowan@ugrad2.cs.jhu.edu>
2018-10-05 21:45:23 -0400

    lzws select_C

--- select_C.sh
+++ select_C.sh
@@ -9,11 +9,11 @@ rm -r out2
 mkdir out1
 mkdir out2
 
-for c in $(seq 0.01 0.01 0.1)
+for c in $(seq 5 1 15)
 do
-	./textcat.py TRAIN loglinear$c chars-10.txt en.1K sp.1K
-	RESULT1=$(./textcat.py TEST loglinear$c chars-10.txt en.1K sp.1K 0.7 esdev/english/*/*)
-	RESULT2=$(./textcat.py TEST loglinear$c chars-10.txt en.1K sp.1K 0.7 esdev/spanish/*/*)
+	./textcat_ensp_c.py TRAIN loglinear$c chars-10.txt en.1K sp.1K
+	RESULT1=$(./textcat_ensp_c.py TEST loglinear$c chars-10.txt en.1K sp.1K 0.7 /home/data/cs465/hw-lm/english_spanish/dev/english/*/*)
+	RESULT2=$(./textcat_ensp_c.py TEST loglinear$c chars-10.txt en.1K sp.1K 0.7 /home/data/cs465/hw-lm/english_spanish/dev/spanish/*/*)
 
 	echo $RESULT1 > out1/$c
 	echo $RESULT2 > out2/$c
